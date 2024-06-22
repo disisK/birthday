0c294cb5102050568533e4b1663717ded8070de8
disisK <126535872+disisK@users.noreply.github.com>
2024-06-23 03:15:41 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,24 +1,24 @@
 let songs = [
     {
-        path: 'file:///C:/Users/ADMIN/Desktop/bday/songs/palagi.mp3',
+        path: 'ongs/palagi.mp3',
 
     },
     {
-        path: 'file:///C:/Users/ADMIN/Desktop/bday/songs/museo.mp3',
+        path: 'songs/museo.mp3',
     },
     {
-        path: 'file:///C:/Users/ADMIN/Desktop/bday/songs/things.mp3',
+        path: 'songs/things.mp3',
     },
     {
-        path: 'file:///C:/Users/ADMIN/Desktop/bday/songs/romcom.mp3',
+        path: 'songs/romcom.mp3',
     },
     {
-        path: 'file:///C:/Users/ADMIN/Desktop/bday/songs/lagi.mp3',
+        path: 'songs/lagi.mp3',
     },
     {
-        path: 'file:///C:/Users/ADMIN/Desktop/bday/songs/things.mp3',
+        path: 'songs/things.mp3',
     },
     {
-        path: 'file:///C:/Users/ADMIN/Desktop/bday/songs/things.mp3',
+        path: 'songs/things.mp3',
     },
 ]
