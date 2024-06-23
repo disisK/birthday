77e3dc61d958548b462bafa0fd3b8349b13a946c
disisK <126535872+disisK@users.noreply.github.com>
2024-06-24 00:42:25 +0900

    Update data.js

--- data.js
+++ data.js
@@ -9,19 +9,13 @@ let songs = [
         path: 'songs/hiraya.mp3',
     },
     {
-        path: 'songs/unti.mp3',
+        path: 'songs/waltz.mp3',
     },
     {
         path: 'songs/ganda.mp3',
     },
     {
-        path: 'songs/waltz.mp3',
-    },
-    {
-        path: 'songs/things.mp3',
-    },
-    {
-        path: 'songs/romcom.mp3',
+        path: 'songs/unti.mp3',
     },
     {
         path: 'songs/lagi.mp3',
@@ -30,10 +24,19 @@ let songs = [
         path: 'songs/romcom.mp3',
     },
     {
-        path: 'songs/lagi.mp3',
+        path: 'dilaw.mp3',
+    },
+    {
+        path: 'songs/come.mp3',
+    },
+    {
+        path: 'songs/she.mp3',
     },
     {
         path: 'songs/things.mp3',
+    },
+    {
+        path: 'things.mp3',
     },
     {
         path: 'songs/things.mp3',
